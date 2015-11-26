1bef8405158c1723e646bb4e49827566f4a9a0f0
BetsME <betsMe@gpbg.info>
2015-11-26 14:37:32 +0200

    Update app.js
    Fix: Double number check

--- app.js
+++ app.js
@@ -498,7 +498,7 @@ var betStore = new Store('bet', {
     }
 
     // Ensure wagerString is a number
-    if (isNaN(n) || /^-?\d+((.\d)?\d*(e[-]?\d)?(\d)*)$/.test(n.toString())) { // - /[^\d]/.test(n.toString())) {
+    if (isNaN(n) || /^\d+.?\d*$/.test(n.toString())) { // - /[^\d]/.test(n.toString())) {
       self.state.wager.error = 'INVALID_WAGER';
       //self.state.wager.error = n.toString();
     // Ensure user can afford balance
