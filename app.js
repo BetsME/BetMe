037e84cbd01485e3cf44db99e4eae4a46193205e
BetsME <betsMe@gpbg.info>
2015-12-02 17:04:31 +0200

    redirect url fix
    Had wrong redirect link....

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfXwhETAAAAAMevh4Ua9wdm2pBWLA9_9ZEMxPqa',  // <----- EDIT ME!
-  redirect_uri: 'https://BetsME.github.io/app.js/',
+  redirect_uri: 'https://BetsME.github.io/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
